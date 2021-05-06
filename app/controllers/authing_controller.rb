ae4d8ffa58c6b99ad8f8611a2580df74c352ce9a
1c7 <guokrfans@gmail.com>
2021-05-06 14:52:14 +0800

    Add 获取 access token

--- app/controllers/authing_controller.rb
+++ app/controllers/authing_controller.rb
@@ -4,6 +4,7 @@ class AuthingController < ApplicationController
 
   # 一些例子
   def example
+    # 初始化
     # options = {
     #   appId: "60800b9151d040af9016d60b",
     #   appHost: "https://rails-demo.authing.cn",
@@ -39,33 +40,36 @@ class AuthingController < ApplicationController
 
   # 回调地址
   def callback
+    # params 长这样
     # render json: params
     # {"code":"Lfu675cmNzgTEePe_SMfsSFp32kQepVKF83JUTqO09-","state":"g60NuMx0H","controller":"authing","action":"callback"}
 
+    # 先拿到 code
     code = params[:code]
-    render json: code
 
-    # options = {
-    #   appId: "60800b9151d040af9016d60b",
-    #   appHost: "https://rails-demo.authing.cn",
-    #   userPoolId: "60800b8ee5b66b23128b4980", 
-    # }
-    # authenticationClient = AuthingRuby::AuthenticationClient.new(options)
+    # 初始化
+    options = {
+      appId: ENV['AUTHING_APP_ID'],
+      secret: ENV['AUTHING_SECRET'],
+      appHost: ENV['AUTHING_APP_HOST'],
+      userPoolId: ENV['AUTHING_USERPOOL_ID'], 
+    }
+    authenticationClient = AuthingRuby::AuthenticationClient.new(options)
 
-    # TODO 用  ENV 提供 secret
-    const authenticationClient = new AuthenticationClient({
-      appId: '应用 ID',
-      secret: '应用密钥',
-      appHost: 'https://{YOUR_DOMAIN}.authing.cn',
-      redirectUri: '业务回调地址',
-    });
-    let res = await authenticationClient.getAccessTokenByCode('授权码 code');
-    let res2 = await authenticationClient.getAccessTokenByCode('授权码 code', {
-      codeVerifier: 'code_challenge 原始值'
-    });
+    # 用 code 换取 token
+    # res = authenticationClient.getAccessTokenByCode(code);
 
-    # TODO  用 code 换取 token
-    # 最后拿到用户信息
+    # 返回示例
+    res = {"status":200,"body":"{\"access_token\":\"eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImZ6S0U4TUlidkVkdl9Mb3N5a1dHTjdNTmpqbjlQMldvVmxtN0pIcFVZUFUifQ.eyJqdGkiOiJsMllkZ0RTZmhkcFhfM3ZGc2dHdlEiLCJzdWIiOiI2MDhiOWI1MjQxNGJkM2I3MWZhZDA0ZWYiLCJpYXQiOjE2MjAyODMxOTMsImV4cCI6MTYyMTQ5Mjc5Mywic2NvcGUiOiJvcGVuaWQgcHJvZmlsZSBwaG9uZSBlbWFpbCIsImlzcyI6Imh0dHBzOi8vcmFpbHMtZGVtby5hdXRoaW5nLmNuL29pZGMiLCJhdWQiOiI2MDgwMGI5MTUxZDA0MGFmOTAxNmQ2MGIifQ.N0s3N9n6RRbu5C1S8VOsIMkFLzv4ph6GOqPsWCdNOxa-Rj-wS3mRteoRs79lGaBtdHuu8pkXNXWARHx8hn6FJVfVZU-ydDZbHu3pY3SBmZtpkiMiFkCdujVLXaMrs5Q_ojMOto9WsABZva-z86fDFwUxhHOZzwMG6eIISAQFjjg2DouOCUxb5f4WuQwTw182EvUtsSrATlMHpYnwnv5TCIcmsxV-dEWC73fNNeQ19dr5JlwvGK2CZRXax5I9WsKlFMBF2c2e1RcSNClfKIL36TwOiHUfl3f74EPMHnBB1ncnbew7ygLGImlXeL27xfLdveXutNioxIKJLY3-y7UdNA\",\"expires_in\":1209600,\"id_token\":\"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2MDhiOWI1MjQxNGJkM2I3MWZhZDA0ZWYiLCJiaXJ0aGRhdGUiOm51bGwsImZhbWlseV9uYW1lIjpudWxsLCJnZW5kZXIiOiJVIiwiZ2l2ZW5fbmFtZSI6bnVsbCwibG9jYWxlIjpudWxsLCJtaWRkbGVfbmFtZSI6bnVsbCwibmFtZSI6bnVsbCwibmlja25hbWUiOm51bGwsInBpY3R1cmUiOiJodHRwczovL2ZpbGVzLmF1dGhpbmcuY28vYXV0aGluZy1jb25zb2xlL2RlZmF1bHQtdXNlci1hdmF0YXIucG5nIiwicHJlZmVycmVkX3VzZXJuYW1lIjpudWxsLCJwcm9maWxlIjpudWxsLCJ1cGRhdGVkX2F0IjoiMjAyMS0wNC0zMFQwNTo1MzoyNi43ODJaIiwid2Vic2l0ZSI6bnVsbCwiem9uZWluZm8iOm51bGwsInBob25lX251bWJlciI6bnVsbCwicGhvbmVfbnVtYmVyX3ZlcmlmaWVkIjpmYWxzZSwiZW1haWwiOiIxQHFxLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwibm9uY2UiOiJJS1FrMF9qWXg4IiwiYXRfaGFzaCI6Ik5sUC1WeHpkRHlmb2hnOUFOR0FBemciLCJhdWQiOiI2MDgwMGI5MTUxZDA0MGFmOTAxNmQ2MGIiLCJleHAiOjE2MjE0OTI3OTMsImlhdCI6MTYyMDI4MzE5MywiaXNzIjoiaHR0cHM6Ly9yYWlscy1kZW1vLmF1dGhpbmcuY24vb2lkYyJ9._7nObGR0XMdCz-uWLiRvHP6OvbaPqwgP50DwaR_ViMo\",\"scope\":\"openid profile phone email\",\"token_type\":\"Bearer\"}","response_headers":{"server":"nginx/1.19.0","date":"Thu, 06 May 2021 06:39:53 GMT","content-type":"application/json; charset=utf-8","transfer-encoding":"chunked","connection":"keep-alive","vary":"Accept-Encoding, Origin","x-powered-by":"Express","pragma":"no-cache","cache-control":"no-cache, no-store","set-cookie":"authing_session=s%3AJXoCkNY7EVApWKnAEKQaZ_c6dXMg95Nb.E6%2FF24JFQ9YoykIVasa07p4sqeg3UdHmcrsP7idOtB4; Path=/; Expires=Thu, 20 May 2021 06:39:53 GMT; HttpOnly; Secure; SameSite=None","strict-transport-security":"max-age=15724800; includeSubDomains"}}
+    # 演示怎么拿到 access_token
+    body_string = res[:body]
+    body = JSON.parse(body_string)
+    access_token = body["access_token"]
+    expires_in = body["expires_in"]
+    id_token = body["id_token"]
+    scope = body["scope"]
+    token_type = body["token_type"]
+    render json: access_token
   end
 
 end
